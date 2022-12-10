4fd8f88ed659b94f1912118d746493d84059d30b
President2010 <adam.delaplanche@gmail.com>
2022-12-10 15:33:01 +0100

    add loop for computing perimetre

--- Calculateur de périmètre.py
+++ Calculateur de périmètre.py
@@ -1,9 +1,22 @@
 print("Bienvenue !")
-operation = input("Entrez le nombre de cotés que comporte ta figure: ")
-x=operation
-a= 500
-z="1+"a
-y=print("Entrez la mesure du segment n°")
-print("Entrez la mesure du segment n°1")
+x = input("Entrez le nombre de cotés que comporte ta figure: ")
+unite=input("Quelle est l'unité de mesure:")
+x = int(x)
+i = 0
+perimetre=0
+
+while i < x:
+    i = i + 1
+
+    # Recuperer la mesure de l'utilisateur
+    c = input("Entrer la valeur du coté "+str(i)+":")
+    c = int(c)
+
+    # Incrémenter la mesure dans le perimetre
+    perimetre = perimetre + c
+
+# Afficher le résultat du périmetre
+print("Le perimètre est de",perimetre,unite)
+
 
 
